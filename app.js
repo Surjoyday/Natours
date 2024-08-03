4102216cf7c4c86aa395de256536cb94936cd286
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-08-03 16:57:34 +0530

    Changed the file structure for modularity

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
+// MIDDLEWARE
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from Middleware 🙌");
+  next();
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+// ROUTES
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
